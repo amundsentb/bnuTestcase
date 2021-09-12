392669446bd4b7257c969bc1509300af7be714cf
Thomas Brecke Amundsen <amundsentb@gmail.com>
2021-09-12 13:24:18 +0200

    loads all feeds

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const linksFeed = require('./feeds');
+const { parse } = require('rss-to-json');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// array of links
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const linksArray = linksFeed.feeds;
+// async await
+let RSSArray = [];
+let 
+(async () => {
+  const linksArrayPromises = linksArray.map(link => {
+    return new Promise(async linkResolve => {
+      const rss = await parse(link);
+      RSSArray.push(rss);
+      linkResolve();
+    });
+  });
+  await Promise.all(linksArrayPromises);
+  console.log(RSSArray);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+})();
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+app.get('/', (req, res) => {
+  res.render('index', {
+     title: 'BNU Testcase',
+     ar
+  });
 });
 
 module.exports = app;
